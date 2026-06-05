7b412ceb8de31e476c12c536ff020a52fec3e551
justin-pruitt <pruittj0823@gmail.com>
2026-06-04 20:21:58 -0400

    Fixed cookie and logout functions, updated table to include edit and delete icons, added user name display and amount of contacts to reflect updates to contacts.html

--- js/code.js
+++ js/code.js
@@ -47,7 +47,7 @@ function doLogin()
 
 				saveCookie();
 	
-				window.location.href = "contacts.html";
+				window.location.href = "/html/contacts.html";
 			}
 		};
 		xhr.send(jsonPayload);
@@ -56,6 +56,8 @@ function doLogin()
 	{
 		document.getElementById("loginResult").innerHTML = err.message;
 	}
+
+	updateUserDisplay();
 
 }
 
@@ -96,7 +98,7 @@ function doRegister() {
 					saveCookie();
 				}
 	
-				window.location.href = "contacts.html";
+				window.location.href = "/html/contacts.html";
 			}
 		};
 		xhr.send(jsonPayload);
@@ -112,14 +114,16 @@ function saveCookie()
 	let minutes = 20;
 	let date = new Date();
 	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+	document.cookie = "firstName=" + firstName + ";expires=" + date.toGMTString();
+	document.cookie = "lastName=" + lastName + ";expires=" + date.toGMTString();
+	document.cookie = "userId=" + userId + ";expires=" + date.toGMTString();
 }
 
 function readCookie()
 {
 	userId = -1;
 	let data = document.cookie;
-	let splits = data.split(",");
+	let splits = data.split(";");
 	for(var i = 0; i < splits.length; i++) 
 	{
 		let thisOne = splits[i].trim();
@@ -140,11 +144,11 @@ function readCookie()
 	
 	if( userId < 0 )
 	{
-		window.location.href = "index.html";
+		window.location.href = "/html/index.html";
 	}
 	else
 	{
-//		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+		updateUserDisplay();
 	}
 }
 
@@ -153,8 +157,10 @@ function doLogout()
 	userId = 0;
 	firstName = "";
 	lastName = "";
-	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "index.html";
+	document.cookie = "firstName=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	document.cookie = "lastName=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	document.cookie = "userId=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "/html/index.html";
 }
 
 function addContact()
@@ -258,14 +264,14 @@ function populateTable(results) {
 			<td>${results[i].Email}</td>
 			<td>${results[i].PhoneNumber}</td>
 			<td>
-				<button onclick="openEditModal(
+				<button class="btn btn--ghost btn--sm" title="Edit contact" onclick="openEditModal(
 				${results[i].ID},
 				'${results[i].FirstName}',
 				'${results[i].LastName}',
 				'${results[i].Email}',
 				'${results[i].PhoneNumber}'
-			)">Edit</button>
-				<button onclick="deleteContact(${results[i].ID})">Delete</button>
+			)"><svg width="16" height="16" viewBox="0 0 16 16" fill="none"><path d="M11.334 2.00001L13.9993 4.66634M2 14H5.33333L13.1333 6.2L10.4673 3.53401L2.66733 11.334L2 14Z" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/></svg><span class="sr-only">Edit</span></button>
+				<button class="btn btn--ghost btn--sm" title="Delete contact" onclick="deleteContact(${results[i].ID})"><svg width="16" height="16" viewBox="0 0 16 16" fill="none"><path d="M2 4H3.33333H14M13.3333 4V12.6667C13.3333 13.0203 13.1929 13.359 12.9428 13.609C12.6928 13.8591 12.354 14 12 14H4C3.64638 14 3.30724 13.8591 3.05719 13.609C2.80714 13.359 2.66667 13.0203 2.66667 12.6667V4M5.33333 4V2.66667C5.33333 2.48986 5.40357 2.32029 5.52859 2.19526C5.65362 2.07024 5.82319 2 6 2H10C10.1768 2 10.3464 2.07024 10.4714 2.19526C10.5964 2.32029 10.6667 2.48986 10.6667 2.66667V4M6.66667 7.33333V11.3333M9.33333 7.33333V11.3333" stroke="currentColor" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/></svg><span class="sr-only">Delete</span></button>
 			</td>
 		</tr>`;
 
@@ -358,4 +364,49 @@ function saveEdit()
 		document.getElementById("contactAddResult").innerHTML = "Edit Failed";
 	}
 	
+}
+
+function updateUserDisplay() {
+    document.getElementById("userName").innerHTML = firstName + " " + lastName;
+}
+
+function updateContactCount(results) {
+    let count = results ? results.length : 0;
+    document.getElementById("contactCount").innerHTML = count + " contact" + (count !== 1 ? "s" : "");
+}
+
+function searchContact(srch = null)
+{
+    if(srch == null) {
+        srch = document.getElementById("searchText").value;
+    }
+
+    let tmp = {search:srch,userId:userId};
+    let jsonPayload = JSON.stringify( tmp );
+
+    let url = urlBase + '/SearchContact.' + extension;
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        xhr.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                document.getElementById("contactSearchResult").innerHTML = "";
+                let jsonObject = JSON.parse( xhr.responseText );
+
+                let results = jsonObject.results || [];
+                populateTable(results);
+                updateContactCount(results);
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        document.getElementById("contactSearchResult").innerHTML = err.message;
+    }
 }
